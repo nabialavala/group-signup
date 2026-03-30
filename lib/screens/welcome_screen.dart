7d600be20d3f60894d8babc7c98d9c472a35dcc6
Sanaa Miller <scmiller024@gmail.com>
2026-03-30 13:46:09 -0400

    Completed welcome screen with animation and navigation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,19 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'signup_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
+  final String? name;
 
-  const WelcomeScreen({
-    super.key, 
-  });
+  const WelcomeScreen({super.key, this.name});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome'),
-      ),
       body: Center(
-        child: Text('Welcome !'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            AnimatedTextKit(
+              animatedTexts: [
+                TypewriterAnimatedText(
+                  name != null
+                      ? 'Welcome $name!'
+                      : 'Welcome to Signup Adventure!',
+                  textStyle: const TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+              totalRepeatCount: 1,
+            ),
+
+            const SizedBox(height: 40),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SignupPage(), // matches partner
+                  ),
+                );
+              },
+              child: const Text("Get Started"),
+            ),
+
+          ],
+        ),
       ),
     );
   }
